70d6b86eb7fcd1fdaca43c5ac64717ce44d5c6e4
irAbs174 <damerchiloa@gmail.com>
2025-02-03 16:32:22 +0330

    +> Debug personnal_type_status field bug in admin.py personnal applicatioin

--- personnal/admin.py
+++ personnal/admin.py
@@ -8,11 +8,11 @@ class DepartmentAdmin(admin.ModelAdmin):
 
 @admin.register(CorporatePersonnel)
 class CorporatePersonnelAdmin(admin.ModelAdmin):
-    list_display = ('personnal_first_name', 'personnal_last_name', 'personnal_email', 'personnal_position', 'personnal_department', 'personnal_type', 'personnal_date_hired')
+    list_display = ('personnal_first_name', 'personnal_last_name', 'personnal_email', 'personnal_position', 'personnal_department', 'personnal_type_status', 'personnal_date_hired')
     list_filter = ('personnal_type_status', 'personnal_department', 'personnal_date_hired')
     search_fields = ('personnal_first_name', 'personnal_last_name', 'personnal_email', 'personnal_position')
     fieldsets = (
         ('اطلاعات شخصی', {'fields': ('personnal_first_name', 'personnal_last_name', 'personnal_email', 'personnal_phone_number', 'personnal_birth_date', 'personnal_address')}),
-        ('اطلاعات شغلی', {'fields': ('personnal_position', 'personnal_department', 'personnal_date_hired', 'personnal_type', 'personnal_salary',)}),
+        ('اطلاعات شغلی', {'fields': ('personnal_position', 'personnal_department', 'personnal_date_hired', 'personnal_type_status', 'personnal_salary',)}),
         ('سایر اطلاعات', {'fields': ('personnal_emergency_contact',)}),
     )
