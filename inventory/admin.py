5d2f670bedff3b6322dd79645eccabbeec02fe80
irAbs174 <damerchiloa@gmail.com>
2025-02-03 16:18:16 +0330

    Write Django admin For inventory and personnal app

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import inventory
 
-# Register your models here.
+@admin.register(inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('inventory_id', 'inventory_name', 'inventory_capacity', 'inventory_location', 'personnal_id', 'created_at', 'updated_at')
+    search_fields = ('inventory_name', 'inventory_id', 'inventory_location')
+    list_filter = ('created_at', 'updated_at')
--- personnal/admin.py
+++ personnal/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Department, CorporatePersonnel
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(CorporatePersonnel)
+class CorporatePersonnelAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'position', 'department', 'employment_status', 'date_hired')
+    list_filter = ('employment_status', 'department', 'date_hired')
+    search_fields = ('first_name', 'last_name', 'email', 'position')
+    fieldsets = (
+        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'birth_date', 'address')}),
+        ('اطلاعات شغلی', {'fields': ('position', 'department', 'date_hired', 'employment_status', 'salary')}),
+        ('سایر اطلاعات', {'fields': ('profile_picture', 'emergency_contact')}),
+    )
